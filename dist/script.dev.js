882b9acda87b8b598a1a4a4d0670c61727edbba7
Adrian Rotila <adrianrotila@gmail.com>
2022-08-26 12:07:23 +0100

    Store the input in a empty array function

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,23 +1,33 @@
 "use strict";
 
 // Bring html elements
-var cells = document.querySelectorAll(".grid__cell");
-var turn = ""; // Function to change the turn
+var cells = document.querySelectorAll(".grid__cell"); // Empty array to store keep track of the board
 
-var changeTurn = function changeTurn(cell) {
-  if (turn === "X") {
-    turn = "O";
-    cell.innerHTML = "O"; // console.log(turn);
+var boardStoredValues = ["", "", "", "", "", "", "", "", ""];
+var currentPlayer = "X"; // Update the Array with the value inserted in a cell
+
+var updateBoard = function updateBoard(index) {
+  boardStoredValues[index] = currentPlayer; // console.log(boardStoredValues);
+}; // Place the correct mark inside the grid element & change the current player
+
+
+var changeTurn = function changeTurn(clickedCell) {
+  if (currentPlayer === "X") {
+    clickedCell.innerHTML = currentPlayer;
+    currentPlayer = "O"; // console.log(currentPlayer);
   } else {
-    turn = "X";
-    cell.innerHTML = "X"; // console.log(turn);
+    clickedCell.innerHTML = currentPlayer;
+    currentPlayer = "X"; // console.log(currentPlayer);
   }
-};
+}; // Main funtction that takes only one click/cell
 
-cells.forEach(function (cell, index) {
-  cell.addEventListener("click", function (e) {
-    // Place the mark inside the grid element
-    changeTurn(cell);
+
+cells.forEach(function (clickedCell, index) {
+  clickedCell.addEventListener("click", function () {
+    // Populate the array with values
+    updateBoard(index); // Display and change the turn
+
+    changeTurn(clickedCell); // Check if there is a winner
   }, {
     once: true
   });
--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 // Bring html elements
 const cells = document.querySelectorAll(".grid__cell");
-let turn = "";
 
-// Function to change the turn
-const changeTurn = (cell) => {
-    if(turn === "X") {
-        turn = "O";
-        cell.innerHTML = "O";
-        // console.log(turn);
+// Empty array to store keep track of the board
+let boardStoredValues = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+
+
+// Update the Array with the value inserted in a cell
+const updateBoard = (index) => {
+    boardStoredValues[index] = currentPlayer;
+    // console.log(boardStoredValues);
+}
+
+// Place the correct mark inside the grid element & change the current player
+const changeTurn = (clickedCell) => {
+    if(currentPlayer === "X") {
+        clickedCell.innerHTML = currentPlayer;
+        currentPlayer = "O";
+        // console.log(currentPlayer);
     } else {
-        turn = "X";
-        cell.innerHTML = "X";
-        // console.log(turn);
+        clickedCell.innerHTML = currentPlayer;
+        currentPlayer = "X";
+        // console.log(currentPlayer);
     }
 }
 
-cells.forEach((cell, index) => {
-    cell.addEventListener("click", (e) => {
-        // Place the mark inside the grid element
+// Main funtction that takes only one click/cell
+cells.forEach((clickedCell, index) => {
+    clickedCell.addEventListener("click", () => {
         
-        changeTurn(cell);
+        // Populate the array with values
+        updateBoard(index);
+        
+        // Display and change the turn
+        changeTurn(clickedCell);
+
+        // Check if there is a winner
+
+        
+
     }, {once: true})
 });
