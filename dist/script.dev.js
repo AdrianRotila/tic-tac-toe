c8050765f32a021e0224c164fb32c1eec86b4ede
Adrian Rotila <adrianrotila@gmail.com>
2022-08-26 16:27:10 +0100

    CSS winning message styling and js bord clear

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,10 +1,14 @@
 "use strict";
 
 // Bring html elements
-var cells = document.querySelectorAll(".grid__cell"); // Empty array to store keep track of the board
+var cells = document.querySelectorAll(".grid__cell");
+var board = document.querySelector(".grid");
+var button = document.querySelector(".reset"); // Empty array to store keep track of the board
 
 var boardStoredValues = ["", "", "", "", "", "", "", "", ""];
-var currentPlayer = "X"; // Update the Array with the value inserted in a cell
+var currentPlayer = "X";
+var gameActive = true;
+var winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [2, 5, 8], [1, 4, 7], [2, 4, 6], [0, 4, 8]]; // Update the Array with the value inserted in a cell
 
 var updateBoard = function updateBoard(index) {
   boardStoredValues[index] = currentPlayer; // console.log(boardStoredValues);
@@ -19,16 +23,62 @@ var changeTurn = function changeTurn(clickedCell) {
     clickedCell.innerHTML = currentPlayer;
     currentPlayer = "X"; // console.log(currentPlayer);
   }
+};
+
+var resultCheck = function resultCheck() {
+  for (var index = 0; index < winConditions.length; index++) {
+    var winSubArray = winConditions[index];
+    var first = boardStoredValues[winSubArray[0]];
+    var second = boardStoredValues[winSubArray[1]];
+    var third = boardStoredValues[winSubArray[2]]; // If empty continue
+
+    if (first === "" || second === "" || third === "") {
+      continue;
+    } // If the entries match break
+
+
+    if (first === second && second === third) {
+      // win message
+      console.log("Player ".concat(first.toString(), " won")); // stop inserting values
+      // clear the board and restart the game
+      // clear the stored values
+      // clear the innerhtml
+
+      gameActive = false; // Clear the board
+
+      clearBoard();
+      break;
+    }
+  } // Tie check
+
+
+  if (!boardStoredValues.includes("")) {
+    console.log("This is a TIE");
+  }
+};
+
+var clearBoard = function clearBoard() {
+  boardStoredValues = ["", "", "", "", "", "", "", "", ""];
+  currentPlayer = "X";
+  button.addEventListener("click", function () {
+    cells.forEach(function (cell) {
+      cell.innerHTML = "";
+    });
+  });
 }; // Main funtction that takes only one click/cell
 
 
-cells.forEach(function (clickedCell, index) {
-  clickedCell.addEventListener("click", function () {
-    // Populate the array with values
-    updateBoard(index); // Display and change the turn
+if (gameActive) {
+  cells.forEach(function (clickedCell, index) {
+    clickedCell.addEventListener("click", function () {
+      // Populate the array with values
+      updateBoard(index); // Display and change the turn
 
-    changeTurn(clickedCell); // Check if there is a winner
-  }, {
-    once: true
+      changeTurn(clickedCell); // Check if there is a winner
+
+      resultCheck();
+    }, {
+      once: true
+    });
   });
-});
+}
--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 // Bring html elements
 const cells = document.querySelectorAll(".grid__cell");
+const board = document.querySelector(".grid");
+const button = document.querySelector(".reset");
 
 // Empty array to store keep track of the board
 let boardStoredValues = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
+let gameActive = true;
 
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [2, 5, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [0, 4, 8],
+];
 
 // Update the Array with the value inserted in a cell
 const updateBoard = (index) => {
@@ -14,6 +27,7 @@ const updateBoard = (index) => {
 
 // Place the correct mark inside the grid element & change the current player
 const changeTurn = (clickedCell) => {
+    
     if(currentPlayer === "X") {
         clickedCell.innerHTML = currentPlayer;
         currentPlayer = "O";
@@ -25,19 +39,73 @@ const changeTurn = (clickedCell) => {
     }
 }
 
+const resultCheck = () => {
+
+    for (let index = 0; index < winConditions.length; index++) {
+        const winSubArray = winConditions[index];
+        const first = boardStoredValues[winSubArray[0]];
+        const second = boardStoredValues[winSubArray[1]];
+        const third = boardStoredValues[winSubArray[2]];
+
+        // If empty continue
+        if(first === "" || second === "" || third === "") {
+            continue;
+        }
+        // If the entries match break
+        if(first === second && second === third) {
+            // win message
+            console.log(`Player ${first.toString()} won`);
+
+            // stop inserting values
+            // clear the board and restart the game
+            // clear the stored values
+            // clear the innerhtml
+            gameActive = false;
+            
+            // Clear the board
+            clearBoard();
+          
+            
+
+            break;
+        }
+    }
+    
+    // Tie check
+    if(!boardStoredValues.includes("")){
+        console.log("This is a TIE");
+    }
+}
+
+const clearBoard = () => {
+    boardStoredValues = ["", "", "", "", "", "", "", "", ""];
+    currentPlayer = "X";
+    button.addEventListener("click", () =>{
+        cells.forEach((cell) => {
+            cell.innerHTML = "";
+        })
+    })
+    
+}
+
 // Main funtction that takes only one click/cell
-cells.forEach((clickedCell, index) => {
-    clickedCell.addEventListener("click", () => {
-        
-        // Populate the array with values
-        updateBoard(index);
-        
-        // Display and change the turn
-        changeTurn(clickedCell);
-
-        // Check if there is a winner
-
-        
-
-    }, {once: true})
-});
+if(gameActive){
+    cells.forEach((clickedCell, index) => {
+        clickedCell.addEventListener("click", () => {
+            
+            // Populate the array with values
+            updateBoard(index);
+            
+            // Display and change the turn
+            changeTurn(clickedCell);
+    
+            // Check if there is a winner
+            resultCheck();
+    
+            
+            
+            
+    
+        }, {once: true})
+    });
+}
